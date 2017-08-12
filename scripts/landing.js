705f39b0920aaf66e6dbd174895bca0cdfa76147
Joshua Horton <jph6482@gmail.com>
2017-08-12 16:53:52 -0400

    checkpoint 9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
     var revealPoint = function(index){
       console.log(index);
@@ -12,7 +12,16 @@ var animatePoints = function() {
     for (var i=0; i<3; i++){
       revealPoint(i);
     };
-<<<<<<< HEAD
-=======
-//comment
->>>>>>> checkpoint-8-dom
+  }
+    window.onload = function() {
+      if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+       var sellingPoints = document.getElementsByClassName('selling-points')[0];
+       var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+              animatePoints(pointsArray);
+          }
+      });
+    }
